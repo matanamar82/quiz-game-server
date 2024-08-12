5914cdf66f92c2fb9b75fab1df4ed69bd2a9705e
assafmendelovich <assafmendelovich@gmail.com>
2024-08-12 11:20:03 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express =require('express')
+
+const app=express()
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World');  
+  });
+
+
+const port = 3150;
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port} :)`);  
+  });
